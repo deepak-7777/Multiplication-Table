b9df595777c26d462dbe7c235db845ce0aa2c4d2
Deepak kumar <globalxwar76@gmail.com>
2025-04-19 22:45:19 +0530

    first Commit

---
+++ app/src/main/java/com/example/tablex/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.tablex;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    ListView listView;
+    SeekBar seekBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        textView = findViewById(R.id.textView);
+        listView = findViewById(R.id.listView);
+        seekBar = findViewById(R.id.seekBar);
+
+        seekBar.setMax(20);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Toast.makeText(MainActivity.this, "The table of " + progress + " is", Toast.LENGTH_SHORT).show();
+                populate(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+    public void populate (int table) {
+        ArrayList<String> mulTable = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            mulTable.add(table + " X " + i + " = " + table*i);
+        }
+        ArrayAdapter<String>arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mulTable);
+        listView.setAdapter(arrayAdapter);
+        textView.setText("Multiplication Table of " + table);
+    }
+}
